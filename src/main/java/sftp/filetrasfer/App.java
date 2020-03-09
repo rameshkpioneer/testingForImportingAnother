2a3af0b1940bfa30d01ddadc8794c00e3b0dd696
Ramesh Kolanuvada <rameshkpioneer@gmail.com>
2020-03-09 11:57:46 +0530

    tetsing for commit new changes

--- src/main/java/sftp/filetrasfer/App.java
+++ src/main/java/sftp/filetrasfer/App.java
@@ -19,7 +19,7 @@ public class App
 			PropertiesUtility propUtil = new PropertiesUtility(); 
 			propUtil.loadPropertiesJSON("D:\\BSDF\\properties.json"); //Local
 			
-			//Adding for testi
+			//Adding for testiff    erer
 			//propUtil.loadPropertiesJSON("./properties.json"); //Server 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
